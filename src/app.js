e66f6196ad4606558f204a1c5c23f2091c06f244
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-04-16 20:46:31 +0300

    Написал логику приложения до отправки на сервер (поведения элементов формы, инпута, кнопки + подготовка структуры вопроса, которая отправится на сервак).

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
+import { isValue } from "./utils";
 import "./style.css";
 
-console.log("Test!");
+const form = document.querySelector("#form");
+const input = form.querySelector("#question-input");
+const submitBtn = form.querySelector("#submit");
+
+form.addEventListener("submit", submitFormHandler);
+
+function submitFormHandler(event) {
+  event.preventDefault();
+
+  if (isValue(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON(),
+    };
+
+    // Чтобы не спамить вопросы. ждем ответа от сервера
+    submitBtn.disabled = true;
+    console.log(question);
+    // async request to server
+
+    // Очищаем поле, убираем ненужные классы, включаем кнопку
+    input.value = "";
+    input.classList = "";
+    submitBtn.disabled = false;
+  }
+}
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export function isValue(value) {
+  return value.length >= 10;
+}
